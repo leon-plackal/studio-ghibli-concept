5a0536007bde38d7a9af8d13aeb0380e7f4fb8c2
Leon Plackal <lpla0005@student.monash.edu>
2024-08-23 21:16:50 +1000

    Adjusted for mobile views

--- src/app/page.js
+++ src/app/page.js
@@ -51,9 +51,9 @@ export default function Home() {
         const x = window.scrollY * speed;
         el.style.transform = `translateX(${-x}px)`;
 
-        if (x > window.innerWidth) {
-          el.style.transform = `translateX(0px)`;
-        }
+        // if (x > window.innerWidth) {
+        //   el.style.transform = `translateX(0px)`;
+        // }
       });
     };
 
@@ -67,22 +67,22 @@ export default function Home() {
 
   return (
     <main className="overflow-hidden">
-      <div className="w-screen h-screen overflow-hidden bg-sky-300 relative">
-        <div className="">
-          <img src="/landing/background-blue.png" alt="hero" />
-          <img src="/landing/mountains.png" alt="hero" className="min-h-screen absolute z-20 top-0 scale-110" data-parallax data-speed="0.01" data-scale="1.1" />
-          <img src="/landing/bear.svg" alt="hero" className="absolute z-30 scale-50 right-40 top-40" data-parallax data-speed="0.01" data-scale="0.5" />
-          <img src="/landing/cloud-1.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-1 " data-parallax data-speed="0.03" />
-          <img src="/landing/cloud-2.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-2 " data-parallax data-speed="0.05" data-scale="0.7" />
-          <img src="/landing/cloud-3.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-3 " />
-          <img src="/landing/cloud-4.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-4 " data-parallax data-speed="0.07" data-scale="0.7" />
-          <img src="/landing/cloud-5.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-5 " data-parallax data-speed="0.00" data-scale="0.7" />
-          <img src="/landing/flower-1.svg" alt="flower" className="absolute z-30 bottom-0 -left-40 scale-[0.7]" data-parallax data-speed="0.01" data-scale="0.7" />
-          <img src="/landing/flower-2.svg" alt="flower" className="absolute z-30 -bottom-48 scale-50" data-parallax data-speed="0.03" data-scale="0.5" />
+      <div className="w-screen h-screen overflow-hidden bg-sky-300 relative flex ">
+        <div className="fixed inset-0 bg-black z-40 opacity-10">
         </div>
 
-        <div className="flex justify-center items-center absolute inset-0 z-40">
-          <div className="flex flex-col items-center fade-in -translate-y-7">
+        <img src="/landing/background-blue.png" alt="hero" className="w-full min-h-screen object-cover " />
+        <img src="/landing/mountains.png" alt="hero" className="inset-0 absolute z-20 w-full min-h-screen object-cover scale-110" data-parallax data-speed="0.01" data-scale="1.1" />
+        {/* <img src="/landing/bear.svg" alt="hero" className="absolute z-30 w-[400px] bottom-0 -right-10 md:right-40" data-parallax data-speed="0.01" data-scale="0.5" /> */}
+        <img src="/landing/cloud-1.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-1 " data-parallax data-speed="0.03" />
+        <img src="/landing/cloud-2.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-2 " data-parallax data-speed="0.05" data-scale="0.7" />
+        <img src="/landing/cloud-4.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-4 " data-parallax data-speed="0.07" data-scale="0.7" />
+        <img src="/landing/cloud-5.svg" alt="cloud" className="absolute z-10 top-0 cloud cloud-5 " data-parallax data-speed="0.00" data-scale="0.7" />
+        <img src="/landing/flower-1.svg" alt="flower" className="absolute z-30 bottom-0 -left-40 scale-[0.7]" data-parallax data-speed="0.01" data-scale="0.7" />
+        <img src="/landing/flower-2.svg" alt="flower" className="absolute z-30 -bottom-48 scale-50" data-parallax data-speed="0.03" data-scale="0.5" />
+
+        <div className="flex justify-center text-center sm:text-left items-center absolute inset-0 z-50">
+          <div className="flex flex-col fade-in justify-center items-center">
             <h3 className="font-medium text-lg">Welcome to</h3>
             <h1 className="text-7xl font-bold">Studio Ghibli</h1>
           </div>
@@ -90,33 +90,53 @@ export default function Home() {
       </div>
 
       <div className="w-screen h-screen bg-sky-300 relative">
-        <img src="/page-2.png" alt="characters" />
-        <img src="/characters-1.png" alt="characters" className="absolute z-30 -top-40 -right-20 w-[600px]" data-scrollup data-speed="0.1" />
-        <img src="/characters-2.png" alt="characters" className="absolute z-30 -top-40 -left-20 w-[600px]" data-scrollup data-speed="0.1" />
-        <img src="/bird-1.png" alt="characters" className="absolute z-30 top-[100px] -right-0 w-[100px]" data-scrollup data-flyacross data-speed="0.1" />
-        <img src="/bird-1.png" alt="characters" className="absolute z-30 top-[300px] -right-0 w-[150px]" data-scrollup data-flyacross data-speed="0.5" />
+        <img src="/page-2.png" alt="characters" className="w-full min-h-screen object-cover -right-[450px] md:-right-0"/>
+        <img src="/characters-1.png" alt="characters" className="absolute z-50 -top-20 sm:-top-40 -right-20 w-[300px] sm:w-[600px]" data-scrollup data-speed="0.1" />
+        <img src="/characters-2.png" alt="characters" className="absolute z-50 -top-20 sm:-top-40 -left-20 w-[300px] sm:w-[600px]" data-scrollup data-speed="0.1" />
+        <img src="/bird-1.png" alt="characters" className="absolute z-30 top-[200px] -right-0 w-[100px]" data-scrollup data-flyacross data-speed="0.1" />
+        <img src="/bird-1.png" alt="characters" className="absolute z-[60] top-[300px] -right-0 w-[150px]" data-scrollup data-flyacross data-speed="0.4" />
         <img src="/bird-2.png" alt="characters" className="absolute z-30 top-[100px] left-[100px] w-[200px]" data-scrollup data-flyacross data-speed="0.3" />
+   
 
-        <div className="absolute flex flex-col justify-center items-center inset-0 z-30">
-          <div className="flex flex-col">
-            <h1 className="text-7xl font-bold">Arriety</h1>
+        <div className="absolute flex flex-col justify-center items-center inset-0 z-50">
+          <div className="flex flex-col px-5">
+            <h1 className="text-7xl font-bold">Arrietty</h1>
             <p className="text-base max-w-80">
-             A secret friendship forms when 12-year-old Shawn meets Arrietty, but their relationship could spell danger for Arrietty's family.
+              A secret friendship forms when 12-year-old Shawn meets Arrietty, but their relationship could spell danger for Arrietty's family.
             </p>
           </div>
         </div>
       </div>
 
       <div className="w-screen h-screen bg-sky-300 relative">
-       <img src="/page-3.png" alt="characters" />
-       <img src="/dragon.png" alt="characters" className="absolute z-30 -top-40 -right-20 w-[600px]" data-scrollup data-speed="0.1"/>
-       <img src="/spirit.png" alt="characters" className="absolute z-30 -bottom-44 -left-20 w-[400px]" data-scrollup data-speed="0.1"/>
+        <img src="/page-3.png" alt="characters" className="w-full min-h-screen object-cover absolute" />
+        <img src="/dragon.png" alt="characters" className="absolute z-30 -top-40 -right-20 w-[600px]" data-scrollup data-speed="0.1" />
 
-       <div className="absolute flex flex-col justify-center items-center inset-0 z-30">
-          <div className="flex flex-col">
+        <div className="absolute flex flex-col justify-center items-center inset-0 z-50">
+          <div className="flex flex-col px-5">
             <h1 className="text-7xl font-bold">Spirited Away</h1>
-            <p className="text-base max-w-96">
+            <p className="text-base max-w-80">
               Chihiro wanders into a magical, bathhouse where she works to free herself and her parents from a witch's spell.
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="w-screen h-screen bg-sky-300 relative">
+        <img src="/page-4.png" alt="characters" className="scale-105 w-full min-h-screen object-cover md:-right-0 absolute" />
+        <img src="/kodama-1.png" alt="characters" className="absolute z-30 -top-40 left-60 w-[100px]" data-scrollup data-speed="0.2" />
+        <img src="/kodama-2.png" alt="characters" className="absolute z-50 -top-0 -left-0 w-[100px]" data-scrollup data-speed="0.2" />
+        <img src="/kodama-3.png" alt="characters" className="absolute z-30 top-40 left-20 w-[100px]" data-scrollup data-speed="0.2" />
+
+        <img src="/spirit.png" alt="characters" className="absolute z-30 -bottom-10 -left-20 w-[250px] sm:w-[400px]" />
+
+        <div className="absolute flex flex-col justify-end items-end inset-0 px-6 py-5 z-50 text-right">
+          <div className="flex flex-col">
+            <h1 className="text-4xl font-bold">by lmnop</h1>
+            <p className="text-base font-mediumD max-w-96">
+              View on
+              <a href="https://dribbble.com/shots/16062004-Studio-Ghibli-Website-Design" className="underline"> Dribbble</a>
             </p>
           </div>
         </div>
